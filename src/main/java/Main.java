0b5acb372335e9e05b4d348d1282c29bac31ce59
Sergei Borisov <bmwth2311@gmail.com>
2024-01-14 11:33:17 +0300

    Проверка на палиндром

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,13 @@
 public class Main {
+    public static boolean isPalindrom(String text){
+        String clean = text.replaceAll("\\s+", "").toLowerCase();
+        StringBuilder plain = new StringBuilder(clean);
+        StringBuilder reverse = plain.reverse();
+        return (reverse.toString()).equals(clean);
+    }
     public static void main(String[] args) {
-
+        if (isPalindrom("А роза упала на лапу Азора") == true) {
+            System.out.println("палиндром");
+        }else System.out.println("не палиндром");
     }
 }
